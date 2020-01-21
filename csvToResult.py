f8875b646a2abb9c13e3d202de37e81e3076a2ce
katsuomi <mugichak1227@gmail.com>
2020-01-21 16:30:42 +0900

    update linter

--- csvToResult.py
+++ csvToResult.py
@@ -3,9 +3,9 @@
 
 import csv
 with open('./keiba1.csv') as f:
-  reader = csv.reader(f)
-  l = [row for row in reader]
-  for num in range(1,21):
-    for num2 in range(num+1,21):
-      result_num = [l[num][3],l[num2][3],1]
-      print(result_num)
+    reader = csv.reader(f)
+    l = [row for row in reader]
+    for num in range(1, 21):
+        for num2 in range(num+1, 21):
+            result_num = [l[num][3], l[num2][3], 1]
+            print(result_num)
--- scrapingToCsv.py
+++ scrapingToCsv.py
@@ -5,31 +5,34 @@ import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-for num in range(1,113):
-  if num > 99:
-   num_str = str(num) 
-  elif num > 9:
-   num_str = '0' + str(num)
-  else:
-   num_str = '00' + str(num)
+for num in range(1, 113):
+    if num > 99:
+        num_str = str(num)
+    elif num > 9:
+        num_str = '0' + str(num)
+    else:
+        num_str = '00' + str(num)
 
-  html = urlopen('http://jra.jp/datafile/seiseki/replay/2014/'+num_str+'.html')
-  bsObj = BeautifulSoup(html, "html.parser")
+    html = urlopen(
+        'http://jra.jp/datafile/seiseki/replay/2014/'+num_str+'.html')
+    bsObj = BeautifulSoup(html, "html.parser")
 
-  table = bsObj.findAll("table",{"bgcolor":"#BABABA"})[0]
-  rows = table.findAll("tr")
+    table = bsObj.findAll("table", {"bgcolor": "#BABABA"})[0]
+    rows = table.findAll("tr")
 
-  csvFile = open("./keiba"+str(num)+".csv",'wt',newline='',encoding='utf-8')
-  writer = csv.writer(csvFile)
-  try:
-    for row in rows:
-      csvRow = []
-      for cell in row.findAll(['td','th']):
-        csvRow.append(cell.get_text())
-          
-      #改行コードが入ってたから改行を消す
-      for x in range(len(csvRow)):
-        csvRow[x]=csvRow[x].replace('\n','')
-            
-      writer.writerow(csvRow)
-  finally:csvFile.close()
+    csvFile = open("./keiba"+str(num)+".csv", 'wt',
+                   newline='', encoding='utf-8')
+    writer = csv.writer(csvFile)
+    try:
+        for row in rows:
+            csvRow = []
+            for cell in row.findAll(['td', 'th']):
+                csvRow.append(cell.get_text())
+
+            # 改行コードが入ってたから改行を消す
+            for x in range(len(csvRow)):
+                csvRow[x] = csvRow[x].replace('\n', '')
+
+            writer.writerow(csvRow)
+    finally:
+        csvFile.close()
