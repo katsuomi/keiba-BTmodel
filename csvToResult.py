2cdb87d1c23e87fb5bfea299a3005f05c7dfee3c
katsuomi <mugichak1227@gmail.com>
2020-01-21 20:50:52 +0900

    update

--- csvToResult.py
+++ csvToResult.py
@@ -2,10 +2,47 @@
 # coding: utf-8
 
 import csv
-with open('./keiba1.csv') as f:
-    reader = csv.reader(f)
-    l = [row for row in reader]
-    for num in range(1, 21):
-        for num2 in range(num+1, 21):
-            result_num = [l[num][3], l[num2][3], 1]
-            print(result_num)
+
+result = []
+
+for i in range(1, 683):
+    i_str = str(i)
+    csv_name = 'keiba' + i_str+'.csv'
+    with open('./'+csv_name) as f:
+        reader = csv.reader(f)
+        l = [row for row in reader]
+        for num in range(1, len(l)):
+            for num2 in range(num+1, len(l)):
+                result.append([l[num][3], l[num2][3], 1])
+
+csvFile = open("./result.csv", 'wt', newline='', encoding='utf-8')
+writer = csv.writer(csvFile)
+try:
+    for row in result:
+        csvRow = []
+        csvRow.append(row)
+        writer.writerow(csvRow)
+finally:
+    csvFile.close()
+
+
+# for文で、配列が既に存在しているのかをチェックする。処理時間が長く、この方法は厳しい。
+# import csv
+
+# result = []
+
+# with open('./keiba1.csv') as f:
+#     reader = csv.reader(f)
+#     l = [row for row in reader]
+#     for num in range(1, len(l)):
+#         for num2 in range(num+1, len(l)):
+#             if len(result) == 0:
+#                 result.append([l[num][3], l[num2][3], 1])
+#             else:
+#                 for i in range(0, len(result)):
+#                     if result[i][0] == l[num][3] and result[i][1] == l[num2][3]:
+#                         result[i][2] += 1
+#                     else:
+#                         result.append([l[num][3], l[num2][3], 1])
+
+# print(result)
--- scrapingToCsv.py
+++ scrapingToCsv.py
@@ -2,37 +2,101 @@
 # coding: utf-8
 
 import csv
+import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-for num in range(1, 113):
-    if num > 99:
-        num_str = str(num)
-    elif num > 9:
-        num_str = '0' + str(num)
-    else:
-        num_str = '00' + str(num)
+html2 = urlopen(
+    'http://jra.jp/datafile/seiseki/g1/hopeful/result/hopeful2018.html')
+bsObj2 = BeautifulSoup(html2, "html.parser")
 
-    html = urlopen(
-        'http://jra.jp/datafile/seiseki/replay/2014/'+num_str+'.html')
-    bsObj = BeautifulSoup(html, "html.parser")
+table = bsObj2.findAll("table", {"class": "striped"})[0]
+rows = table.findAll("tr")
+csvFile = open("./keiba682.csv", 'wt', newline='', encoding='utf-8')
+writer = csv.writer(csvFile)
+try:
+    for row in rows:
+        csvRow = []
+        for cell in row.findAll(['td', 'th']):
+            csvRow.append(cell.get_text())
 
-    table = bsObj.findAll("table", {"bgcolor": "#BABABA"})[0]
-    rows = table.findAll("tr")
+        # 改行コードが入ってたから改行を消す
+        for x in range(len(csvRow)):
+            csvRow[x] = csvRow[x].replace('\n', '')
 
-    csvFile = open("./keiba"+str(num)+".csv", 'wt',
-                   newline='', encoding='utf-8')
-    writer = csv.writer(csvFile)
-    try:
-        for row in rows:
-            csvRow = []
-            for cell in row.findAll(['td', 'th']):
-                csvRow.append(cell.get_text())
+        writer.writerow(csvRow)
+finally:
+    csvFile.close()
 
-            # 改行コードが入ってたから改行を消す
-            for x in range(len(csvRow)):
-                csvRow[x] = csvRow[x].replace('\n', '')
 
-            writer.writerow(csvRow)
-    finally:
-        csvFile.close()
+# import csv
+# import re
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+
+# num = 627
+# html = urlopen('http://jra.jp/datafile/seiseki/replay/2017/g1.html')
+# bsObj = BeautifulSoup(html, "html.parser")
+# for a in bsObj.find_all('a', href=re.compile("result")):
+#     g1_url = 'http://jra.jp' + a.get('href')
+#     html2 = urlopen(g1_url)
+#     bsObj2 = BeautifulSoup(html2, "html.parser")
+
+#     table = bsObj2.findAll("table", {"bgcolor": "#BABABA"})[0]
+#     rows = table.findAll("tr")
+#     num_str = str(num)
+#     csvFile = open("./keiba"+num_str+".csv", 'wt',
+#                    newline='', encoding='utf-8')
+#     writer = csv.writer(csvFile)
+#     try:
+#         for row in rows:
+#             csvRow = []
+#             for cell in row.findAll(['td', 'th']):
+#                 csvRow.append(cell.get_text())
+
+#             # 改行コードが入ってたから改行を消す
+#             for x in range(len(csvRow)):
+#                 csvRow[x] = csvRow[x].replace('\n', '')
+
+#             writer.writerow(csvRow)
+#     finally:
+#         csvFile.close()
+#     num = num + 1
+
+
+# G2 ~ G3のレースの場合、以下でOKだが、G1のレースは上記で実装　
+# import csv
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+
+# for num in range(1, 113):
+#     if num > 99:
+#         num_str = str(num)
+#     elif num > 9:
+#         num_str = '0' + str(num)
+#     else:
+#         num_str = '00' + str(num)
+
+#     html = urlopen(
+#         'http://jra.jp/datafile/seiseki/replay/2018/'+num_str+'.html')
+#     bsObj = BeautifulSoup(html, "html.parser")
+
+#     table = bsObj.findAll("table", {"class": "mainList"})[0]
+#     rows = table.findAll("tr")
+
+#     num2 = str(num+446)
+#     csvFile = open("./keiba"+num2+".csv", 'wt', newline='', encoding='utf-8')
+#     writer = csv.writer(csvFile)
+#     try:
+#         for row in rows:
+#             csvRow = []
+#             for cell in row.findAll(['td', 'th']):
+#                 csvRow.append(cell.get_text())
+
+#             # 改行コードが入ってたから改行を消す
+#             for x in range(len(csvRow)):
+#                 csvRow[x] = csvRow[x].replace('\n', '')
+
+#             writer.writerow(csvRow)
+#     finally:
+#         csvFile.close()
