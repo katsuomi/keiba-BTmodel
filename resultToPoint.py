faefb198d3ed8c3e3b16eaa13f94b588a270f1a4
青木克臣 <mugichak1227@gmail.com>
2020-01-25 18:19:38 +0900

    update

--- resultToPoint.py
+++ resultToPoint.py
@@ -3,10 +3,17 @@
 
 import csv
 
-result = []
 with open('./result.csv') as f:
     reader = csv.reader(f)
+    index = 0 
     l = [row for row in reader]
+    for x in range(0,len(l)):
+        for y in range(index+1,len(l)):
+            if l[x][0] == l[y][0] and l[x][1] == l[y][1]:
+                l[y][0] = "none"
+                l[x][2] = str(int(l[x][2]) + 1)
+        index += 1
+
     # 買った馬の表示 l[0][0]
     # 負けた馬の表示 l[0][1]
     # 数値の表示 l[0][2]
@@ -16,7 +23,9 @@ writer = csv.writer(csvFile)
 try:
     for row in l:
         csvRow = []
-        csvRow.append(row)
+        csvRow.append(row[0])
+        csvRow.append(row[1])
+        csvRow.append(row[2])
         writer.writerow(csvRow)
 finally:
     csvFile.close()
