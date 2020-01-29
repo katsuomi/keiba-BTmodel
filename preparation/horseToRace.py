14557a55ca8d63d51abdf4aba0cc1114e5e0fe46
青木克臣 <mugichak1227@gmail.com>
2020-01-29 20:39:49 +0900

    update

---
+++ preparation/horseToRace.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import csv
+
+result = []
+
+# resultに馬名:初期値(1)の辞書を作成
+csvfile = open('./csv/allHorses.csv')
+for row in csv.reader(csvfile):
+  with open('./csv/horseCompetitionTable.csv') as f:
+    reader = csv.reader(f)
+    l = [row2 for row2 in reader]
+    result = [s for s in l if row[0] in s[0]]
+  csv_name = row[0]+'.csv'
+  csvFile = open('./horse_csv/'+csv_name, 'wt', newline='', encoding='utf-8')
+  writer = csv.writer(csvFile)
+  try:
+    for row in result:
+      csvRow = []
+      csvRow.append(row[0])
+      csvRow.append(row[1])
+      writer.writerow(csvRow)
+  finally:
+    csvFile.close()
